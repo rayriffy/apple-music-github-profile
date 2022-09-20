dae2c544a38c72ccb1f970e7a06f2ebaf4f9a1a6
Phumrapee Limpianchop <git@rayriffy.com>
2022-09-20 19:56:32 +0900

    chore: staged changes

--- src/core/services/getMusicKitDeveloperToken.ts
+++ src/core/services/getMusicKitDeveloperToken.ts
@@ -1,21 +1,25 @@
 import jwt from 'jsonwebtoken'
 
-const { 
-  APPLE_PRIVATE_KEY,
-  APPLE_KEY_ID,
-  APPLE_TEAM_ID,
-} = process.env
+const { APPLE_PRIVATE_KEY, APPLE_KEY_ID, APPLE_TEAM_ID } = process.env
 
 export const getMusicKitDeveloperToken = (keyDuration = '1h') => {
-  const token = jwt.sign({}, APPLE_PRIVATE_KEY, {
-    issuer: APPLE_TEAM_ID,
-    expiresIn: keyDuration,
-    algorithm: 'ES256',
-    header: {
-      alg: 'ES256',
-      kid: APPLE_KEY_ID
+  const token = jwt.sign(
+    process.env.NODE_ENV === 'production'
+      ? {
+          origin: ['https://apple-music.rayriffy.com'],
+        }
+      : {},
+    APPLE_PRIVATE_KEY,
+    {
+      issuer: APPLE_TEAM_ID,
+      expiresIn: keyDuration,
+      algorithm: 'ES256',
+      header: {
+        alg: 'ES256',
+        kid: APPLE_KEY_ID,
+      },
     }
-  })
+  )
 
   return token
 }

b04e7260a05eb3d1c9bba39873a8ec08a7ff9e1b
Phumrapee Limpianchop <git@rayriffy.com>
2022-10-08 13:10:35 +0900

    feat: axiom

--- next.config.js
+++ next.config.js
@@ -1,23 +1,26 @@
 const withPreact = require('next-plugin-preact')
+const { withAxiom } = require('next-axiom')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
 /** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer(
-  withPreact({
-    async rewrites() {
-      return [
-        {
-          source: '/theme/sample.svg',
-          destination: '/api/svg?username=rayriffy',
-        },
-        {
-          source: '/theme/default.svg:path*',
-          destination: '/api/svg:path*',
-        },
-      ]
-    },
-    output: 'standalone',
-  })
+module.exports = withAxiom(
+  withBundleAnalyzer(
+    withPreact({
+      async rewrites() {
+        return [
+          {
+            source: '/theme/sample.svg',
+            destination: '/api/svg?username=rayriffy',
+          },
+          {
+            source: '/theme/default.svg:path*',
+            destination: '/api/svg:path*',
+          },
+        ]
+      },
+      output: 'standalone',
+    })
+  )
 )

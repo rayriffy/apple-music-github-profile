0bdc37e759e4b5b2b366597a642322e8067197ee
Phumrapee Limpianchop <git@rayriffy.com>
2022-10-09 21:02:26 +0900

    fix: deploys.app incorrently serialize new line in env

--- src/core/services/getMusicKitDeveloperToken.ts
+++ src/core/services/getMusicKitDeveloperToken.ts
@@ -3,16 +3,16 @@ import jwt from 'jsonwebtoken'
 const { RIFFY_DEBUG, APPLE_PRIVATE_KEY, APPLE_KEY_ID, APPLE_TEAM_ID } = process.env
 
 export const getMusicKitDeveloperToken = (keyDuration = 60 * 60) => {
-  if (RIFFY_DEBUG === 'true') {
-    console.log({
-      APPLE_PRIVATE_KEY,
-      APPLE_KEY_ID,
-      APPLE_TEAM_ID,
-    })
-  }
+  // if (RIFFY_DEBUG === 'true') {
+  //   console.log({
+  //     APPLE_PRIVATE_KEY,
+  //     APPLE_KEY_ID,
+  //     APPLE_TEAM_ID,
+  //   })
+  // }
 
   const token = jwt.sign({},
-    APPLE_PRIVATE_KEY,
+    APPLE_PRIVATE_KEY.replaceAll(/\\n/g, "\n"),
     {
       issuer: APPLE_TEAM_ID,
       expiresIn: keyDuration,

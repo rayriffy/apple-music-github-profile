7f2d9e09a74fb427fc7b9cdea89d37bf84c828ba
Phumrapee Limpianchop <git@rayriffy.com>
2022-10-13 03:56:07 +0900

    chore: log stripping

--- src/core/services/getMusicKitDeveloperToken.ts
+++ src/core/services/getMusicKitDeveloperToken.ts
@@ -1,17 +1,9 @@
 import jwt from 'jsonwebtoken'
 
-const { RIFFY_DEBUG, APPLE_PRIVATE_KEY, APPLE_KEY_ID, APPLE_TEAM_ID } =
+const { APPLE_PRIVATE_KEY, APPLE_KEY_ID, APPLE_TEAM_ID } =
   process.env
 
 export const getMusicKitDeveloperToken = (keyDuration = 60 * 60) => {
-  // if (RIFFY_DEBUG === 'true') {
-  //   console.log({
-  //     APPLE_PRIVATE_KEY,
-  //     APPLE_KEY_ID,
-  //     APPLE_TEAM_ID,
-  //   })
-  // }
-
   const token = jwt.sign({}, APPLE_PRIVATE_KEY.replaceAll(/\\n/g, '\n'), {
     issuer: APPLE_TEAM_ID,
     expiresIn: keyDuration,

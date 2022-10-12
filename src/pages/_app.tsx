156a073aacc30103cd865d309faa2571503b9fc4
Phumrapee Limpianchop <git@rayriffy.com>
2022-10-13 02:13:17 +0900

    feat: footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import Head from 'next/head'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 
+import { FaGithub } from 'react-icons/fa'
+
 import '../styles/tailwind.css'
 
 const App: NextPage<AppProps> = props => {
@@ -26,6 +28,12 @@ const App: NextPage<AppProps> = props => {
         </section>
         <Component {...pageProps} />
       </main>
+      <footer className='text-center text-gray-600 py-6 space-y-2'>
+        <div className="flex justify-center">
+          <a href="https://github.com/rayriffy/apple-music-github-profile" target="_blank" rel="noopener noreferer"><FaGithub className="w-6 h-6" /></a>
+        </div>
+        <p className="text-sm">© {new Date().getFullYear()} Phumrapee Limpianchop</p>
+      </footer>
     </Fragment>
   )
 }

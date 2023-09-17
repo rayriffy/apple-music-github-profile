2358f3deff0a9ed1ce011c82c294f0179380ab29
Phumrapee Limpianchop <git@rayriffy.com>
2023-09-18 00:43:13 +0900

    refactor: deps bump

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  semi: false,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+}

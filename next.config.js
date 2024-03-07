75a06755120b24c545b5b07648a9667b7a443bde
Phumrapee Limpianchop <git@rayriffy.com>
2024-03-08 01:32:59 +0900

    fix: console

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ module.exports = withBundleAnalyzer({
   },
   compiler: {
     reactRemoveProperties: true,
-    removeConsole: true,
+    removeConsole: false,
   },
   experimental: {
     typedRoutes: true,
